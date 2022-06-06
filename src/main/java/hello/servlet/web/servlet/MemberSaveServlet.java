596cdc3109b1ae283d21cee0c537355fcf439c53
roats <kimjinho123@naver.com>
2022-06-06 15:39:26 +0900

    Changes

--- src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servlet/MemberSaveServlet.java
@@ -19,13 +19,10 @@ public class MemberSaveServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("MemberSaveServlet.service");
-
         String username = request.getParameter("username");
         int age = Integer.parseInt(request.getParameter("age"));
 
         Member member = new Member(username, age);
-
         memberRepository.save(member);
 
         response.setContentType("text/html");
